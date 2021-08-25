2fd19bbce7cb0ec636d9ed72bfae710e637b5c00
Bruno <brunopixel@gmail.com>
2021-08-24 21:01:55 -0300

    - criado index.php para controle das paginas internas (iniciado as chamadas do bootstrap) - criado diretório pages - criado diretório images - instalado bootstrap 5 (CSS e JS) - criado diretorio function, onde irá conter a funçoes em php - criado diretorio dados onde irá conter as arrays

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <title>1645027-5-BrunoCabral</title>
+
+  </head>
+  <body>
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
